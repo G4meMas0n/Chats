aac8469d078a5e12c52d73a763ecd5207172c9a7
G4meMason <g4memas0n@gmail.com>
2020-08-01 10:31:06 +0200

    Added UTF-8 support for properties files

--- Chats/src/main/java/de/g4memas0n/chats/messaging/Messages.java
+++ Chats/src/main/java/de/g4memas0n/chats/messaging/Messages.java
@@ -7,13 +7,19 @@ import org.jetbrains.annotations.Nullable;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
 import java.util.Collection;
+import java.util.List;
 import java.util.Locale;
 import java.util.MissingResourceException;
+import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
 /**
@@ -57,8 +63,10 @@ public final class Messages {
     }
 
     public synchronized void setLocale(@NotNull final Locale locale) {
+        ResourceBundle.clearCache();
+
         try {
-            this.localBundle = ResourceBundle.getBundle(BUNDLE_BASE, locale);
+            this.localBundle = ResourceBundle.getBundle(BUNDLE_BASE, locale, new CustomPropertiesControl());
 
             if (this.localBundle.getLocale().equals(locale)) {
                 this.logger.debug("Loaded resource bundle for language: " + locale);
@@ -74,7 +82,7 @@ public final class Messages {
         try {
             this.customBundle = ResourceBundle.getBundle(BUNDLE_BASE, locale,
                     new CustomFileClassLoader(this.getClass().getClassLoader(), this.directory),
-                    ResourceBundle.Control.getNoFallbackControl(ResourceBundle.Control.FORMAT_PROPERTIES));
+                    new CustomNoFallbackControl());
 
             if (this.customBundle.getLocale().equals(locale)) {
                 this.logger.debug("Detected and loaded custom resource bundle for language: " + locale);
@@ -218,6 +226,67 @@ public final class Messages {
                 }
             }
 
+            return null;
+        }
+    }
+
+    /**
+     * Custom PropertiesControl for reading properties files as UTF-8 instead of ISO-8859-1.
+     */
+    private static class CustomPropertiesControl extends ResourceBundle.Control {
+
+        @Override
+        public @NotNull List<String> getFormats(@NotNull final String baseName) {
+            return ResourceBundle.Control.FORMAT_PROPERTIES;
+        }
+
+        @Override
+        public @Nullable ResourceBundle newBundle(@NotNull final String baseName,
+                                                  @NotNull final Locale locale,
+                                                  @NotNull final String format,
+                                                  @NotNull final ClassLoader loader,
+                                                  final boolean reload) throws IOException {
+            final String resource = this.toResourceName(this.toBundleName(baseName, locale), "properties");
+
+            ResourceBundle bundle = null;
+            InputStream stream = null;
+
+            if (reload) {
+                final URL url = loader.getResource(resource);
+
+                if (url != null) {
+                    final URLConnection connection = url.openConnection();
+
+                    if (connection != null) {
+                        connection.setUseCaches(false);
+
+                        stream = connection.getInputStream();
+                    }
+                }
+            } else {
+                stream = loader.getResourceAsStream(resource);
+            }
+
+            if (stream != null) {
+                try {
+                    bundle = new PropertyResourceBundle(new InputStreamReader(stream, StandardCharsets.UTF_8));
+                } finally {
+                    stream.close();
+                }
+            }
+
+            return bundle;
+        }
+    }
+
+    /**
+     * Custom PropertiesControl for using no fallback locales.
+     */
+    private static class CustomNoFallbackControl extends CustomPropertiesControl {
+
+        @Override
+        public @Nullable Locale getFallbackLocale(@NotNull final String baseName,
+                                                  @NotNull final Locale locale) {
             return null;
         }
     }
